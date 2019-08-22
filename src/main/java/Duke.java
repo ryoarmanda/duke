6b74e878f89a8fa2c0228bd088339106096a34bc
Ryo Chandra Putra Armanda <ryoarmanda@rocketmail.com>
2019-08-22 17:07:30 +0800

    Implement Level 3 - Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final int MAX_TASKS = 100;
     private Task[] tasks;
     private int totalTasks;
 
     public Duke() {
-        this.tasks = new Task[100];
+        this.tasks = new Task[MAX_TASKS];
         this.totalTasks = 0;
     }
 
@@ -27,28 +28,60 @@ public class Duke {
     }
 
     private void displayTasks() {
-        String[] formatted = new String[this.totalTasks];
+        String[] responses = new String[this.totalTasks + 1];
+
+        responses[0] = "Here are the tasks in your list:";
         for (int i = 0; i < this.totalTasks; i++) {
-            formatted[i] = String.format("%d. %s", i + 1, this.tasks[i].getDescription());
+            responses[i + 1] = String.format("%d.%s", i + 1, this.tasks[i]);
         }
-        displayResponse(formatted);
+
+        displayResponse(responses);
+    }
+
+    private void markTaskDone(int taskIndex) {
+        Task task = this.tasks[taskIndex];
+        task.markAsDone();
+        displayResponse(new String[]{
+            "Nice! I've marked this task as done:",
+            "  " + task
+        });
+    }
+
+    private void addTask(String description) {
+        this.tasks[this.totalTasks++] = new Task(description);
+        displayResponse("added: " + description);
+    }
+
+    private boolean handleInput(String input) {
+        String[] tokens = input.split(" ");
+        String cmd = tokens[0];
+
+        switch (cmd) {
+            case "bye":
+                return true;
+            case "list":
+                displayTasks();
+                return false;
+            case "done":
+                markTaskDone(Integer.parseInt(tokens[1]) - 1);
+                return false;
+            default:
+                addTask(input);
+                return false;
+        }
     }
 
     private void run() {
         Scanner sc = new Scanner(System.in);
 
-        displayResponse(new String[]{"Hello! I'm Duke", "What can I do for you?"});
+        displayResponse(new String[]{
+            "Hello! I'm Duke",
+            "What can I do for you?"
+        });
 
-        String cmd = sc.nextLine();
-        while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {
-                displayTasks();
-            } else {
-                this.tasks[this.totalTasks++] = new Task(cmd);
-                displayResponse("added: " + cmd);
-            }
-
-            cmd = sc.nextLine();
+        boolean stop = false;
+        while (!stop) {
+            stop = handleInput(sc.nextLine());
         }
 
         displayResponse("Bye. Hope to see you again soon!");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,26 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
     }
 
     public String getDescription() {
         return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return this.isDone ? "\u2714" : "\u2718";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.getDescription());
     }
 }
