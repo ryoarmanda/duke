f454b607006882c47de7087f7e4fc07095be4db2
Ryo Chandra Putra Armanda <ryoarmanda@rocketmail.com>
2019-08-28 22:44:51 +0800

    Implement Level 7 - Save

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,6 +6,10 @@ public class Deadline extends Task {
         this.time = time;
     }
 
+    public String getTime() {
+        return this.time;
+    }
+
     @Override
     public String toString() {
         return String.format("[D]%s (by: %s)", super.toString(), this.time);
